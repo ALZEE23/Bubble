6a90695e4e9e06aef0890b02e456236360569ddb
ALZEE23 <muhamadalfarizial726@gmail.com>
2025-01-25 21:20:45 +0700

    refactor: interact

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -5,14 +5,90 @@ using UnityEngine;
 public class Interact : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int trash;
+    public int toy;
+    public int bin;
+    private bool isInTrigger = false;
+    private GameObject currentObject;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isInTrigger && Input.GetMouseButton(0))
+        {
+            if (currentObject != null && currentObject.tag == "trash")
+            {
+                Debug.Log("Trash");
+                // Assuming you have a variable to keep track of trash count
+                trash++;
+                Destroy(currentObject);
+                currentObject = null;
+                isInTrigger = false;
+            }
+
+            if (currentObject != null && currentObject.tag == "toy")
+            {
+                Debug.Log("Toy");
+                // Assuming you have a variable to keep track of trash count
+                toy++;
+                Destroy(currentObject);
+                currentObject = null;
+                isInTrigger = false;
+            }
+
+            if (currentObject != null && currentObject.tag == "bin")
+            {
+                Debug.Log("Bin");
+                if (trash > 0)
+                {
+                    trash--;
+                }
+                // if (toy > 0)
+                // {
+                //     toy--;
+                // }
+                
+                bin++;
+                // Destroy(currentObject);
+                currentObject = null;
+                isInTrigger = false;
+            }
+        }
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+        if (other.gameObject.tag == "trash")
+        {
+            isInTrigger = true;
+            currentObject = other.gameObject;
+        }
+
+        if (other.gameObject.tag == "toy")
+        {
+            isInTrigger = true;
+            currentObject = other.gameObject;
+        }
+
+        if (other.gameObject.tag == "bin")
+        {
+            isInTrigger = true;
+            currentObject = other.gameObject;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == currentObject)
+        {
+            isInTrigger = false;
+            currentObject = null;
+        }
     }
 }
